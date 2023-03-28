8b1e758ee4e2ee5e348aaae6cbeed0c4a68f547f
Waldo <oswaldo.ct18@gmail.com>
2023-03-28 16:36:53 -0700

    Module el boton

--- balance/lib/utils/constants.dart
+++ balance/lib/utils/constants.dart
@@ -8,4 +8,22 @@ class Constants {
           topLeft: Radius.circular(30), topRight: Radius.circular(30)),
     );
   }
+
+  static customButton(
+      String _text, double _height, Color color, void Function() ontap) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: ontap,
+      child: Container(
+        height: _height,
+        color: color,
+        child: Center(
+          child: Text(
+            _text,
+            style: const TextStyle(fontSize: 27),
+          ),
+        ),
+      ),
+    );
+  }
 }
